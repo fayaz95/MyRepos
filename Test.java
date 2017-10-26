4d87ef58dab4d2125619af4e86262c4449d23dce
Shaik <fayaz.a.shaik@capgemini.com>
2017-10-26 15:54:16 +0530

    Change made

--- Test.java
+++ Test.java
@@ -2,10 +2,6 @@ public class Test{
 
 public static void main(String args[]){
 
-<<<<<<< HEAD
-=======
-system.out.print("Hello World");
-system.out.print("Hello World World");
->>>>>>> tester
+system.out.println(" welcome ");
 }
 }
